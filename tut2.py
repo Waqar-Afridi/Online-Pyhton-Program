27cd052337321819d89a5085e1ce0d8dcca288a6
WAQRAN AFRI <waqarafridi54@gmail.com>
2021-10-23 21:08:32 +0500

    work Accomplish

---
+++ tut2.py
@@ -0,0 +1,67 @@
+#
+
+# assignment 3
+
+'''Twinkle, twinkle, little star,
+ How I wonder what you are!
+ Up above the world so high,
+Like a diamond in the sky.
+ Twinkle, twinkle, little star,
+ How I wonder what you are'''
+
+# print("Twinkle,twinkle,little start,")
+# print("\t\t How I wonder what you are!")
+# print("\t\t\t\t Up above the world so high,")
+# print("\t\t\t\t Like a diamond in the sky.")
+# print("Twinkle,twinkle,little start,")
+# print("\t\t How I wonder what you are!")
+
+# 2
+# import sys
+# print(sys.version)
+
+# 4
+# r = float(input("Enter the radius of the circle: "))
+# area =( 3.1416 * r)
+# print("Radius:", r)
+# print("Area = ", area)
+
+
+# a = int(input("Enter the marks out of 100 : "))
+#
+# b = int(input("Enter the marks out of 100 : "))
+
+# c = int(input("Enter the marks out of 100 : "))
+#
+# d = int(input("Enter the marks out of 100 : "))
+#
+# e = int(input("Enter the marks out of 100 : "))
+# #Then I'll add all the variables
+#
+# total = a+ b + c + d + e
+# #Finding the percentage
+#
+# percent = total/500*100
+# #Printing the total and percent variable.
+#
+# if(percent>=90):
+#     print("Grade: A")
+# elif percent>=80 and percent<90:
+#     print("Grade: B")
+# elif percent>=70 and percent<80:
+#     print("Grade: C")
+# elif percent>=60 and percent<70:
+#     print("Grade: D")
+# else:
+#     print("Grade: F")
+#
+# print("The total mark is ", total, "out of 500.")
+#
+# print("Percentage is ", percent, "%.")
+
+# studentGrades = [9.1, 8.8, 10.0, 7.7, 6.8, 8.0, 10.0, 8.1, 10.0, 9.9]
+# print( "The length of List is :",len(studentGrades))
+
+# Question 10
+listt = [1, 2, 3]
+print(sum(listt))
